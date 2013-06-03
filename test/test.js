252279aad628c4b958507a226a9e87bf45b646c5
Soares Chen <quiverjs@soareschen.com>
2013-06-04 00:17:31 +0200

    Added router handler factory creation.

--- test/test.js
+++ test/test.js
@@ -1,14 +1,17 @@
 
-var router = require('../lib/router')
-var should = require('should')
-var createEmptyReadStream = require('quiver-stream-channel').createEmptyReadStream
-var streamConvert = require('quiver-stream-convert')
+"use strict"
 
-var async = require('async')
+var should = require('should')
+var router = require('../lib/router')
+var async = require('quiver-async-tasks')
+var streamConvert = require('quiver-stream-convert')
+var streamChannel = require('quiver-stream-channel')
+
+var createEmptyStreamable = streamChannel.createEmptyStreamable
 
 describe('route matcher test', function() {
   it('regex matcher test', function() {
-    var matcher = router.createRegexMatcher(/^\/hello(\/\w+)$/, ['name'])
+    var matcher = router.createRegexRouteMatcher(/^\/hello(\/\w+)$/, ['name'])
 
     var result1 = matcher('/dont-exist')
     should.not.exist(result1)
@@ -32,14 +35,14 @@ describe('router tests', function() {
       }
     }
 
-    var config = { staticRoutes:  staticRoutes }
-    router.createRouterHandler(config, function(err, routeHandler) {
+    var routeSpecs = { staticRoutes:  staticRoutes }
+    router.createRouterHandler(routeSpecs, function(err, routeHandler) {
       should.not.exist(err)
 
-      async.parallel([
+      async.parallelArray([
         function(callback) {
           var args = { path: '/foo' }
-          routeHandler(args, createEmptyReadStream(), function(err, resultStreamable) {
+          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
             should.not.exist(err)
 
             streamConvert.streamableToText(resultStreamable, function(err, result) {
@@ -51,13 +54,13 @@ describe('router tests', function() {
         },
         function(callback) {
           var args = { path: '/blah'}
-          routeHandler(args, createEmptyReadStream(), function(err, resultStreamable) {
+          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
             err.errorCode.should.equal(404)
             callback()
           })
         },
         function(callback) {
-          routeHandler({ }, createEmptyReadStream(), function(err, resultStreamable) {
+          routeHandler({ }, createEmptyStreamable(), function(err, resultStreamable) {
             err.errorCode.should.equal(400)
             callback()
           })
@@ -78,7 +81,7 @@ describe('router tests', function() {
 
     var dynamicRoutes = [
       {
-        matcher: router.createRegexMatcher(/^\/hello(\/\w+)$/, ['path']),
+        matcher: router.createRegexRouteMatcher(/^\/hello(\/\w+)$/, ['path']),
         handler: function(args, streamable, callback) {
           args.path.should.equal('/world')
           callback(null, streamConvert.textToStreamable('hello handler'))
@@ -86,7 +89,7 @@ describe('router tests', function() {
       },
       {
         // override /foo handler
-        matcher: router.createRegexMatcher(/^\/foo$/),
+        matcher: router.createRegexRouteMatcher(/^\/foo$/),
         handler: function(args, streamable, callback) {
           throw new Error('should never called')
         }
@@ -107,10 +110,10 @@ describe('router tests', function() {
     router.createRouterHandler(config, function(err, routeHandler) {
       should.not.exist(err)
 
-      async.parallel([
+      async.parallelArray([
         function(callback) {
           var args = { path: '/hello/world' }
-          routeHandler(args, createEmptyReadStream(), function(err, resultStreamable) {
+          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
             should.not.exist(err)
 
             streamConvert.streamableToText(resultStreamable, function(err, result) {
@@ -124,7 +127,7 @@ describe('router tests', function() {
         function(callback) {
           var args = { path: '/foo' }
 
-          routeHandler(args, createEmptyReadStream(), function(err, resultStreamable) {
+          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
             should.not.exist(err)
 
             streamConvert.streamableToText(resultStreamable, function(err, result) {
@@ -138,7 +141,7 @@ describe('router tests', function() {
         function(callback) {
           var args = { path: '/dont-exist' }
 
-          routeHandler(args, createEmptyReadStream(), function(err, resultStreamable) {
+          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
             should.not.exist(err)
 
             streamConvert.streamableToText(resultStreamable, function(err, result) {
@@ -149,6 +152,133 @@ describe('router tests', function() {
             })
           })
         }
+      ], callback)
+    })
+  })
+})
+
+describe('route handler factory test', function() {
+  it('basic factory test', function(callback) {
+    var routeSpecs = [
+      {
+        type: 'static',
+        path: '/foo',
+        handlerFactory: function(config, callback) {
+          var fooValue = config.fooConfig
+          fooValue.should.equal('foo')
+
+          // try to temper config
+          config.barConfig = 'foo'
+
+          var handler = function(args, streamable, callback) {
+            callback(null, streamConvert.textToStreamable(fooValue))
+          }
+
+          callback(null, handler)
+        }
+      },
+      {
+        type: 'static',
+        path: '/bar',
+        handlerFactory: function(config, callback) {
+          var barValue = config.barConfig
+          barValue.should.equal('bar')
+
+          // try to temper config
+          config.fooConfig = 'bar'
+
+          var handler = function(args, streamable, callback) {
+            callback(null, streamConvert.textToStreamable(barValue))
+          }
+
+          callback(null, handler)
+        }
+      },
+      {
+        type: 'dynamic',
+        matcher: router.createRegexRouteMatcher(/^\/hello(\/\w+)$/, ['path']),
+        handlerFactory: function(config, callback) {
+          var handler = function(args, streamable, callback) {
+            args.path.should.equal('/world')
+            callback(null, streamConvert.textToStreamable('hello world'))
+          }
+
+          callback(null, handler)
+        }
+      },
+      {
+        type: 'default',
+        handlerFactory: function(config, callback) {
+          var handler = function(args, streamable, callback) {
+            callback(null, streamConvert.textToStreamable('default handler'))
+          }
+
+          callback(null, handler)
+        }
+      }
+    ]
+
+    var routeHandlerFactory = router.createRouterHandlerFactory(routeSpecs)
+    var config = {
+      fooConfig: 'foo',
+      barConfig: 'bar'
+    }
+    routeHandlerFactory(config, function(err, handler) {
+      if(err) throw err
+
+      async.parallelArray([
+        function(callback) {
+          var args = { path: '/foo' }
+          handler(args, createEmptyStreamable(), function(err, resultStreamable) {
+            if(err) throw err
+
+            streamConvert.streamableToText(resultStreamable, function(err, result) {
+              if(err) throw err
+
+              result.should.equal('foo')
+              callback()
+            })
+          })
+        },
+        function(callback) {
+          var args = { path: '/bar' }
+          handler(args, createEmptyStreamable(), function(err, resultStreamable) {
+            if(err) throw err
+
+            streamConvert.streamableToText(resultStreamable, function(err, result) {
+              if(err) throw err
+
+              result.should.equal('bar')
+              callback()
+            })
+          })
+        },
+        function(callback) {
+          var args = { path: '/hello/world' }
+          handler(args, createEmptyStreamable(), function(err, resultStreamable) {
+            if(err) throw err
+
+            streamConvert.streamableToText(resultStreamable, function(err, result) {
+              if(err) throw err
+
+              result.should.equal('hello world')
+              callback()
+            })
+          })
+        },
+        function(callback) {
+          var args = { path: '/not-found' }
+          handler(args, createEmptyStreamable(), function(err, resultStreamable) {
+            if(err) throw err
+
+            streamConvert.streamableToText(resultStreamable, function(err, result) {
+              if(err) throw err
+
+              result.should.equal('default handler')
+              callback()
+            })
+          })
+        },
       ], callback)
     })
   })
