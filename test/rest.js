fb859e2461f353986f1b995eb3a5e6624d4acb30
Soares Chen <soares.chen@gmail.com>
2014-05-18 04:58:50 +0800

    Rewriting quiver-router library.

---
+++ test/rest.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var routeSpecs = [
+  {
+    routeType: 'static',
+    path: '/foo',
+    handlers: {
+      'GET': 'foo get handler',
+      'POST': 'foo post handler'
+    }
+  }
+]
