9371439ab8302826bbe8b35197f4aaa8260c11fd
Soares Chen <soares.chen@gmail.com>
2014-05-19 04:51:14 +0800

    More work in progress.

--- test/test.js
+++ test/test.js
@@ -1,286 +1,82 @@
-
 'use strict'
 
-var should = require('should')
-var router = require('../lib/router')
-var async = require('quiver-async-tasks')
-var streamConvert = require('quiver-stream-convert')
-var streamChannel = require('quiver-stream-channel')
+var fooHandleableBuilder = function(config, callback) {
 
-var createEmptyStreamable = streamChannel.createEmptyStreamable
+}
 
-describe('route matcher test', function() {
-  it('regex matcher test', function() {
-    var matcher = router.createRegexRouteMatcher(/^\/hello(\/\w+)$/, ['name'])
+var barHandleableBuilder = function(config, callback) {
+  
+}
 
-    var result1 = matcher('/dont-exist')
-    should.not.exist(result1)
-
-    var result2 = matcher('/hello/world')
-    should.exist(result2)
-    result2.name.should.equal('/world')
-  })
-})
-
-describe('router tests', function() {
-  it('static route test', function(callback) {
-    var fooCalled = false
-    var staticRoutes = {
-      '/foo': function(args, streamable, callback) {
-        fooCalled = true
-        callback(null, streamConvert.textToStreamable('foo handler'))
-      },
-      '/bar': function(args, streamable, callback) {
-        throw new Error('should never reached')
-      }
-    }
-
-    var routeIndex = { staticRoutes:  staticRoutes }
-    router.createRouterHandler(routeIndex, function(err, routeHandler) {
-      should.not.exist(err)
-
-      async.parallelArray([
-        function(callback) {
-          var args = { path: '/foo' }
-          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
-            should.not.exist(err)
-
-            streamConvert.streamableToText(resultStreamable, function(err, result) {
-              should.not.exist(err)
-              result.should.equal('foo handler')
-              callback()
-            })
-          })
-        },
-        function(callback) {
-          var args = { path: '/blah'}
-          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
-            err.errorCode.should.equal(404)
-            callback()
-          })
-        },
-        function(callback) {
-          routeHandler({ }, createEmptyStreamable(), function(err, resultStreamable) {
-            err.errorCode.should.equal(400)
-            callback()
-          })
-        }
-      ], callback)
-    })
-  })
-
-  it('dynamic route test', function(callback) {
-    var staticRoutes = {
-      '/foo': function(args, streamable, callback) {
-        callback(null, streamConvert.textToStreamable('static foo handler'))
-      },
-      '/bar': function(args, streamable, callback) {
-        throw new Error('should never called')
-      }
-    }
-
-    var dynamicRoutes = [
+var routeSpecs = [
+  {
+    routeType: 'static',
+    path: '/foo',
+    handlers: [
       {
-        matcher: router.createRegexRouteMatcher(/^\/hello(\/\w+)$/, ['path']),
-        handler: function(args, streamable, callback) {
-          args.path.should.equal('/world')
-          callback(null, streamConvert.textToStreamable('hello handler'))
-        }
+        method: 'GET',
+        handleable: fooGetHandleable
       },
       {
-        // override /foo handler
-        matcher: router.createRegexRouteMatcher(/^\/foo$/),
-        handler: function(args, streamable, callback) {
-          throw new Error('should never called')
-        }
+        method: 'POST',
+        handleable: fooPostHandleable
       }
     ]
+  },
+  {
+    routeType: 'param',
+    path: '/bar/:name',
+    handleable: barHandleable
+  }
+]
 
-    var defaultHandler = function(args, streamable, callback) {
-      args.path.should.equal('/dont-exist')
-      callback(null, streamConvert.textToStreamable('default handler'))
-    }
-
-    var routeIndex = {
-      staticRoutes: staticRoutes,
-      dynamicRoutes: dynamicRoutes,
-      defaultRoute: defaultHandler
-    }
-
-    router.createRouterHandler(routeIndex, function(err, routeHandler) {
-      should.not.exist(err)
-
-      async.parallelArray([
-        function(callback) {
-          var args = { path: '/hello/world' }
-          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
-            should.not.exist(err)
-
-            streamConvert.streamableToText(resultStreamable, function(err, result) {
-              should.not.exist(err)
-
-              result.should.equal('hello handler')
-              callback()
-            })
-          })
-        },
-        function(callback) {
-          var args = { path: '/foo' }
-
-          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
-            should.not.exist(err)
-
-            streamConvert.streamableToText(resultStreamable, function(err, result) {
-              should.not.exist(err)
-
-              result.should.equal('static foo handler')
-              callback()
-            })
-          })
-        },
-        function(callback) {
-          var args = { path: '/dont-exist' }
-
-          routeHandler(args, createEmptyStreamable(), function(err, resultStreamable) {
-            should.not.exist(err)
-
-            streamConvert.streamableToText(resultStreamable, function(err, result) {
-              should.not.exist(err)
-
-              result.should.equal('default handler')
-              callback()
-            })
-          })
-        }
-      ], callback)
-    })
-  })
-})
-
-describe('route handler factory test', function() {
-  it('basic factory test', function(callback) {
-    var routeList = [
+var routeBuildSpecs = [
+  {
+    routeType: 'static',
+    path: '/foo',
+    handleableBuilders: [
       {
-        routeType: 'static',
-        path: '/foo',
-        handlerBuilder: function(config, callback) {
-          var fooValue = config.fooConfig
-          fooValue.should.equal('foo')
-
-          // try to temper config
-          config.barConfig = 'foo'
-
-          var handler = function(args, streamable, callback) {
-            callback(null, streamConvert.textToStreamable(fooValue))
-          }
-
-          callback(null, handler)
-        }
+        method: 'GET',
+        handleableBuilder: fooGetHandleableBuilder
       },
       {
-        routeType: 'static',
-        path: '/bar',
-        handlerBuilder: function(config, callback) {
-          var barValue = config.barConfig
-          barValue.should.equal('bar')
-
-          // try to temper config
-          config.fooConfig = 'bar'
-
-          var handler = function(args, streamable, callback) {
-            callback(null, streamConvert.textToStreamable(barValue))
-          }
-
-          callback(null, handler)
-        }
-      },
-      {
-        routeType: 'regex',
-        regex: /^\/hello(\/\w+)$/, 
-        matchFields: ['path'],
-        handlerBuilder: function(config, callback) {
-          var handler = function(args, streamable, callback) {
-            args.path.should.equal('/world')
-            callback(null, streamConvert.textToStreamable('hello world'))
-          }
-
-          callback(null, handler)
-        }
-      },
-      {
-        routeType: 'default',
-        handlerBuilder: function(config, callback) {
-          var handler = function(args, streamable, callback) {
-            callback(null, streamConvert.textToStreamable('default handler'))
-          }
-
-          callback(null, handler)
-        }
+        method: 'POST',
+        handleableBuilder: fooPostHandleableBuilder
       }
     ]
+  },
+  {
+    routeType: 'param',
+    path: '/bar/:name',
+    handleableBuilder: barHandleableBuilder
+  }
+]
 
-    var routerHandlerBuilder = router.createRouterHandlerBuilder(routeList)
-    var config = {
-      fooConfig: 'foo',
-      barConfig: 'bar'
+var routeListComponent = {
+  name: 'test route list',
+  type: 'route list',
+  routeList: [
+    {
+      routeType: 'static',
+      path: '/foo',
+      handlers: [
+        {
+          method: 'GET',
+          handler: 'foo get handler'
+        },
+        {
+          method: 'POST',
+          handler: 'foo post handler'
+        }
+      ]
+    },
+    {
+      routeType: 'param',
+      path: '/bar/:name',
+      handler: 'bar handler'
     }
-    routerHandlerBuilder(config, function(err, handler) {
-      if(err) throw err
-
-      async.parallelArray([
-        function(callback) {
-          var args = { path: '/foo' }
-          handler(args, createEmptyStreamable(), function(err, resultStreamable) {
-            if(err) throw err
-
-            streamConvert.streamableToText(resultStreamable, function(err, result) {
-              if(err) throw err
-
-              result.should.equal('foo')
-              callback()
-            })
-          })
-        },
-        function(callback) {
-          var args = { path: '/bar' }
-          handler(args, createEmptyStreamable(), function(err, resultStreamable) {
-            if(err) throw err
-
-            streamConvert.streamableToText(resultStreamable, function(err, result) {
-              if(err) throw err
-
-              result.should.equal('bar')
-              callback()
-            })
-          })
-        },
-        function(callback) {
-          var args = { path: '/hello/world' }
-          handler(args, createEmptyStreamable(), function(err, resultStreamable) {
-            if(err) throw err
-
-            streamConvert.streamableToText(resultStreamable, function(err, result) {
-              if(err) throw err
-
-              result.should.equal('hello world')
-              callback()
-            })
-          })
-        },
-        function(callback) {
-          var args = { path: '/not-found' }
-          handler(args, createEmptyStreamable(), function(err, resultStreamable) {
-            if(err) throw err
-
-            streamConvert.streamableToText(resultStreamable, function(err, result) {
-              if(err) throw err
-
-              result.should.equal('default handler')
-              callback()
-            })
-          })
-        },
-      ], callback)
-    })
-  })
-})
+  ],
+  middlewares: [
+    'foo middleware'
+  ]
+}
